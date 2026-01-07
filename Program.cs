57cdb8032122bd220174f939808d82a344595068
khaledeldsoky <khlaedmohamedeldsoky@gmail.com>
2026-01-07 18:27:33 +0200

    Add controller mapping and update API port in Docker Compose

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddScoped<SingUpServices>();
 
@@ -41,6 +42,7 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.MapControllers();
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
