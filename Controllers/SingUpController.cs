1e4d660d5ca0cf9b79d014819082f023f5a95040
khaledeldsoky <khlaedmohamedeldsoky@gmail.com>
2026-01-07 20:44:58 +0200

    Add email and phone validation in SingUpController

--- Controllers/SingUpController.cs
+++ Controllers/SingUpController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using App.Services;
 using App.Models;
+using System.Net.Mail;
 
 namespace App.Controllers
 {
@@ -16,9 +17,43 @@ namespace App.Controllers
       _signUpServices = signUpServices;
     }
 
+    bool IsValidEmail(string Mail)
+    {
+      try
+      {
+        var Email = new MailAddress(Mail);
+        return true;
+      }
+      catch (System.Exception)
+      {
+        return false;
+      }
+    }
+
+    bool IsValidPhone(string Phone)
+    {
+      if (string.IsNullOrWhiteSpace(Phone)) return false;
+      if (Phone.Length != 11) return false;
+      if (!Phone.All(char.IsDigit)) return false;
+      string[] EgyptionPrefix = {"010","011","012","015"};
+      foreach (var item in Phone)
+      {
+        if (Phone.StartsWith(item))
+        {
+          return true;
+        }
+      }
+      return false; 
+    }
+
     [HttpPost("SingUp")]
     public IActionResult AddUser(SingUpModel user)
     {
+      if (!IsValidEmail(user.email))
+        return BadRequest("Invalid email format");
+        
+      if (IsValidPhone(user.phone))
+        return BadRequest("Invalid phone format");
       try
       {
         _signUpServices.AddUser(user);
